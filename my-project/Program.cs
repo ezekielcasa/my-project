8e2c2ecdfa607aece51ce419b7db389f7cb5a9da
ezekiel <ezekiel@mycasabank.com>
2025-04-14 14:49:46 +0100

    first commit

---
+++ my-project/Program.cs
@@ -0,0 +1,15 @@
+namespace my_project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
